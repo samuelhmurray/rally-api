f985487de354c0136b59973e4492097df952b163
Sam Murray <shm31094@gmail.com>
2024-04-24 15:45:12 -0400

    get all needs works

--- rallyproject/urls.py
+++ rallyproject/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
+from rallyapi.views import NeedViewSet
+
 
 router = routers.DefaultRouter(trailing_slash=False)
+router.register(r"need", NeedViewSet, "need")
+
 
 urlpatterns = [
     path('', include(router.urls)),
