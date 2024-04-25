4bdb0f351a7b5992c0f1336c2a2b8499c49c2f48
Sam Murray <shm31094@gmail.com>
2024-04-25 15:08:40 -0400

    works in postman i think - now lets see if it works when i add to client

--- rallyproject/urls.py
+++ rallyproject/urls.py
@@ -1,14 +1,16 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
-from rallyapi.views import NeedViewSet
+from rallyapi.views import NeedViewSet, UserViewSet
 
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r"needs", NeedViewSet, "need")
 
-
 urlpatterns = [
-    path('', include(router.urls)),
+    path("", include(router.urls)),
+    path("login", UserViewSet.as_view({"post": "user_login"}), name="login"),
+    path(
+        "register", UserViewSet.as_view({"post": "register_account"}), name="register"
+    ),
 ]
-
