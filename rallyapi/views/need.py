5fb3765828d56799231634f50345981615a39f27
Sam Murray <shm31094@gmail.com>
2024-04-26 15:58:22 -0400

    http://localhost:8000/needs/n works

--- rallyapi/views/need.py
+++ rallyapi/views/need.py
@@ -1,4 +1,4 @@
-from rest_framework import serializers, viewsets
+from rest_framework import serializers, viewsets, permissions
 from rest_framework.response import Response
 from ..models import Community, Need
 from django.contrib.auth.models import User
@@ -6,14 +6,12 @@ from django.contrib.auth.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email']  # Include more fields as needed
-
+        fields = ['id', 'username', 'email']
 
 class CommunitySerializer(serializers.ModelSerializer):
     class Meta:
         model = Community
         fields = '__all__'
-
 
 class NeedSerializer(serializers.ModelSerializer):
     user = UserSerializer()
@@ -23,9 +21,20 @@ class NeedSerializer(serializers.ModelSerializer):
         model = Need
         fields = ['id', 'description', 'date_posted', 'complete', 'user', 'community']
 
-
 class NeedViewSet(viewsets.ViewSet):
+    permission_classes = [permissions.IsAuthenticated]
+
     def list(self, request):
         needs = Need.objects.all()
+        serializer = NeedSerializer(needs, many=True, context={'request': request})
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        try:
+            user_id = int(pk)
+        except ValueError:
+            return Response({'error': 'Invalid user ID'}, status=status.HTTP_400_BAD_REQUEST)
+
+        needs = Need.objects.filter(user_id=user_id)
         serializer = NeedSerializer(needs, many=True, context={'request': request})
         return Response(serializer.data)
